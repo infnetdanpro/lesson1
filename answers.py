fd82c1e3e5aed343f411241748b4449bfbad12bb
Maksim Artemev <dan-pro352@ya.ru>
2017-11-18 18:04:34 +0300

    Бот новый

--- answers.py
+++ answers.py
@@ -1,5 +1,5 @@
 def get_answer(question):
-	answers = {'привет':'И тебе привет!', 'как дела':'Лучше всех', 'пока':'Увидимся'}
-	return answers.get(question, 'Задайте вопрос еще раз').lower()
+    answers = {'привет':'И тебе привет!', 'как дела':'Лучше всех', 'пока':'Увидимся'}
+    return answers.get(question, 'Задайте вопрос еще раз').lower()
 question = input('Введите текст: ')
-print(get_answer(question))
+print(get_answer(question)) 
